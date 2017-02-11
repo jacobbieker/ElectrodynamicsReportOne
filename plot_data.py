d19d7e58b4604020c6945b9ab0524178b8826646
Jacob Bieker <jacob@bieker.tech>
2017-02-11 20:36:56 +0100

    Moved files around to make it easier to find
    Converting from verbose to not verbose is going to be frustrating...

--- plot_data.py
+++ plot_data.py
@@ -1,3 +1,10 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+
+for file in os.listdir(os.path.join(".", "SIMIONRUNS", "partTwo")):
+    print(file)
+    with open(os.path.join(".", "SIMIONRUNS", "partTwo", file), 'r') as data_source:
+        for line in data_source:
+            print(line)
