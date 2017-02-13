dcbb999169983ef9a278407bc8e73bbc8cbdc306
Jacob Bieker <jacob@bieker.tech>
2017-02-13 18:08:45 +0100

    Sorta got plotting working
    Some odd results with two values being much lower than they should be based on the trends

--- plot_data.py
+++ plot_data.py
@@ -12,8 +12,8 @@ def determine_t_over_u(kinetic_energy, charge, potential_lens):
 
 data = {}
 counter = 0
-plotting_two_data = [[],[],[],[],[],[],[],[],[]]
-plotting_t_over_u = [[],[],[],[],[],[],[],[],[]]
+plotting_two_data = [[],[],[],[],[],[],[],[],[], []]
+plotting_t_over_u = [[],[],[],[],[],[],[],[],[], []]
 for file in os.listdir(os.path.join(".", "SIMIONRUNS", "partTwo")):
     print(file)
     with open(os.path.join(".", "SIMIONRUNS", "partTwo", file), 'r') as data_source:
@@ -50,6 +50,77 @@ for file in os.listdir(os.path.join(".", "SIMIONRUNS", "partTwo")):
                     data[counter][inner_counter]["Event"] = "Created"
     pprint(data)
 
+# Get the delimited data
+two_delimited_data = {}
+two_delimited_data['1'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['2'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['3'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['4'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['5'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['6'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['7'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['8'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['9'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['10'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['11'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['12'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['13'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['14'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['15'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['16'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['17'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['18'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['19'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['20'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['21'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['22'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['23'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['24'] = {'Focal': {}, 'Start': {}}
+two_delimited_data['25'] = {'Focal': {}, 'Start': {}}
+
+for file in os.listdir(os.path.join(".", "SIMIONRUNS", "delimitedTwo")):
+    print(file)
+    with open(os.path.join(".", "SIMIONRUNS", "delimitedTwo", file), 'r') as data_source:
+        two_delimited_data['13'] = {'Focal': {}, 'Start': {}}
+        # Get info from title
+        counter += 1
+        list_name = file.split("_")
+        data[counter] = {"KE": float(list_name[1].split("E")[1]), "POT": list_name[2], "L": list_name[3], "Q": float(list_name[4].split("Q")[1])}
+        inner_counter = 0
+        reader = csv.DictReader(data_source, delimiter=",")
+        # Get info from title
+        for row in reader:
+            if row["Events"] == '2048':
+                two_delimited_data[row["Ion N"]]['Focal'] = {"X": float(row["X"]), "Charge": float(row["Charge"]), "KE": float(row["KE"])}
+            elif row["Events"] == '1':
+                two_delimited_data[row["Ion N"]]['Start'] = {"Y": float(row["Y"]), "Charge": float(row["Charge"]), "KE": float(row["KE"])}
+
+    # now add in data from the delimited stuff, hoepfully some points line up
+    del two_delimited_data['13']
+    for key in two_delimited_data:
+        if -8 <= int(two_delimited_data[key]['Start']['Y']) <= 9:
+            spot = -1
+            if int(two_delimited_data[key]['Start']['Y']) == -8:
+                spot = 1
+            elif int(two_delimited_data[key]['Start']['Y']) == -6:
+                spot = 2
+            elif int(two_delimited_data[key]['Start']['Y']) == -4:
+                spot = 3
+            elif int(two_delimited_data[key]['Start']['Y']) == -2:
+                spot = 4
+            elif int(two_delimited_data[key]['Start']['Y']) == 2:
+                spot = 6
+            elif int(two_delimited_data[key]['Start']['Y']) == 4:
+                spot = 7
+            elif int(two_delimited_data[key]['Start']['Y']) == 6:
+                spot = 8
+            elif int(two_delimited_data[key]['Start']['Y']) == 8:
+                spot = 9
+            if spot != -1:
+                plotting_two_data[spot].append((float(two_delimited_data[key]['Focal']['X']) - 54.0)/14.0)
+                plotting_t_over_u[spot].append(-1.0 * determine_t_over_u(two_delimited_data[key]['Start']["KE"], two_delimited_data[key]['Start']["Charge"], 10.0))
+
+
 # Convert data from above to get what we need: where the Y axis was crossed, already have KE, Charge, Potential from filename
 # Need to get data only from every ion that has the same start position, so 9 tries per file
 # If Ion = 1 it starts at the same place each time, so don't need to check that, only where crossed Y axis
@@ -59,10 +130,11 @@ for key in data:
         if isinstance(data[key][key2], dict):
             if "Ion" in data[key][key2].keys() and "Event" in data[key][key2].keys():
                 if data[key][key2]["Ion"] == current_ion:
-                    print("Start new ion")
                     plotting_two_data[current_ion].append((float(data[key][key2]["X"].split(" ")[0]) - 54.0)/14.0)
                     plotting_t_over_u[current_ion].append(determine_t_over_u(data[key]["KE"], data[key]["Q"], 10.0))
-                    current_ion += 1
+                current_ion += 1
+
+
 print(plotting_two_data)
 print(plotting_t_over_u)
 potential = 10.0
